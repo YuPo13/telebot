ea1a7def2f28d91af155bb2122c5c0cc8b8973c4
YuPo13 <yuliya.povzlo@gmail.com>
2020-02-17 13:53:04 +0200

    /exchange script logic changed v.1

--- app.py
+++ app.py
@@ -16,16 +16,18 @@ app = Flask(__name__)
 
 def get_rates():
     contents = requests.get('https://api.exchangeratesapi.io/latest?base=USD').json()
-    rates = contents['rates']
+    return contents['rates']
+
+
+def list_output(rates_obtained):
     list_rates = """"""
-    for item in rates.items():
+    for item in rates_obtained.items():
         list_rates += f"{item[0]}: {item[1]: .2f} \n"
     return list_rates
 
 
 def calculate_amount(amount, ccy):
-    contents = requests.get('https://api.exchangeratesapi.io/latest?base=USD').json()
-    rates = contents['rates']
+    rates = get_rates()
     result = amount * float(rates[f'{ccy}'])
     return result
 
@@ -43,28 +45,21 @@ def respond():
            The bot is using European Central Bank data to provide you recent USD-based currency exchange rates.
            Please use following commands:
            /list   - to obtain full list of all currencies exchanged recently listed at ECB
+           /exchange USD 10 to CAD - to calculate amount of destination currency (CAD in example) 
+           equal to set amount of USD (10 in example)
            """
         bot.send_message(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id)
     elif text == "/list":
         rates = get_rates()
-        bot.send_message(chat_id=chat_id, text=rates, reply_to_message_id=msg_id)
-    elif text == "/exchange":
-        instructions = """Please enter: 
-                        amount of USD to be exchanged (positive integer)
-                        to
-                        destination currency code (3 capitalized letters). 
-                        E.g. in order to exchange USD 10 to CAD, enter 
-                        10 to CAD 
-                        """
-        bot.send_message(chat_id=chat_id, text=instructions, reply_to_message_id=msg_id,
-                         reply_markup=telegram.ForceReply())
-        new_update = telegram.Update.de_json(request.get_json(force=True), bot)
-        exchange_input = new_update.message.text.encode('utf-8').decode()
-        exch_details = exchange_input.split()
+        result = list_output(rates)
+        bot.send_message(chat_id=chat_id, text=result, reply_to_message_id=msg_id)
+    elif text.startswith("/exchange"):
         try:
+            exch_input = text.split()
             rates = get_rates()
-            if isinstance(int(exch_details[0]), int) and exch_details[2] in rates:
-                result = calculate_amount(exch_details[0], exch_details[2])
+            if exch_input[1] == "USD" and isinstance(exch_input[2],int) \
+                    and exch_input[3] == "to" and exch_input[4] in rates:
+                result = calculate_amount(exch_input[2], exch_input[4])
                 bot.send_message(chat_id=chat_id, text=f"USD{exch_details[0]} are {exch_details[2]}{result}",
                                  reply_to_message_id=msg_id)
         except (ValueError, TypeError):
